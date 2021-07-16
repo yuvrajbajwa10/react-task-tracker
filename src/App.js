67717ffe7dc65359b29f6b56e38172d6d2047004
Yuvraj Bajwa <yuvrajbajwa8@gmail.com>
2021-07-16 23:06:56 +1200

    removed Spaces

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
--- src/components/About.js
+++ src/components/About.js
@@ -15,3 +15,4 @@ const About = () => {
 }
 
 export default About
+    
