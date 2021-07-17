78ef9a62600a2cdb8f7c9245cbf3cebe25927427
Yuvraj Bajwa <yuvrajbajwa8@gmail.com>
2021-07-17 13:39:50 +1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -67,15 +67,13 @@ function App() {
       reminder: !taskToToggle.reminder
     }
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
       method: 'PUT',
       headers: {
         'Content-type': 'application/json'
       },
       body: JSON.stringify(updTask)
     })
-
-    const data = await res.json()
 
     setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
 
